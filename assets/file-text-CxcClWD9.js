ca96f622ffcb1cc94d24b17f5606b85a08b50f61
Abdul Hadhi <108044807+hadhi-dev@users.noreply.github.com>
2026-01-19 23:08:11 +0530

    Update the UI

---
+++ assets/file-text-CxcClWD9.js
@@ -0,0 +1,11 @@
+import{c as a}from"./createLucideIcon-DTte54Zk.js";/**
+ * @license lucide-react v0.483.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M12 7v14",key:"1akyts"}],["path",{d:"M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z",key:"ruj8y"}]],h=a("BookOpen",e);/**
+ * @license lucide-react v0.483.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z",key:"1rqfz7"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"M10 9H8",key:"b1mrlr"}],["path",{d:"M16 13H8",key:"t4e002"}],["path",{d:"M16 17H8",key:"z1uh3a"}]],p=a("FileText",t);export{h as B,p as F};
---
+++ assets/inheritsLoose-MEHl_3-6.js
@@ -0,0 +1,1 @@
+import{k as e}from"./memoize-one.esm-DQq2xVVF.js";function p(o,t){o.prototype=Object.create(t.prototype),o.prototype.constructor=o,e(o,t)}export{p as _};
